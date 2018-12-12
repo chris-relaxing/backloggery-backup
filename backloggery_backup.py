d870dd4115d1c9ee83daca5ed4ad2279344938cf
Chris Nielsen <chris.nielsen@forumcomm.com>
2018-12-12 16:55:45 -0600

    Added detection for console, status, and name.

--- backloggery_backup.py
+++ backloggery_backup.py
@@ -1,4 +1,4 @@
-import requests
+# import requests
 
 """
 You don't need to know ahead of time how many games there are in your collection. This script will collect
@@ -22,7 +22,8 @@ export_to_xlsx = True
 
 count = 0
 iterations = 1
-backloggery_backup = ""
+# backloggery_backup = ""
+backloggery_backup = []
 
 moreGames = True
 while moreGames:
@@ -31,17 +32,19 @@ while moreGames:
     #targetURL = "https://backloggery.com/ajax_moregames.php?user=originalKILLJOY&ajid=" + str(count)
     r = requests.get(targetURL)
     page_source = r.text
-    #page_source = page_source.split('\n')
-    print("Length of page_source", len(page_source))
+    page_source = page_source.strip('\t\t').split('\n')
+#     print("Length of page_source", len(page_source))
 
     count += 50
 
-    print("iterations ", iterations)
+#     print("iterations ", iterations)
     iterations  += 1
 
     backloggery_backup = backloggery_backup + page_source
+    backloggery_csv = []
 
-    if(len(page_source) < 100):
+    if(count >= 1000):
+#     if(len(page_source) < 100):
     #if(count >= 3850):
         moreGames = False
 
@@ -51,9 +54,39 @@ while moreGames:
 
         print("Number of lines in the returned response: ", len(page_source))
         print("Number of games total", iterations*50)
-        print("Final page_source\n", page_source)
+#         print("Final page_source\n", page_source)
 
         print("--------------------------------------")
 
         #This works:
-        #print(backloggery_backup)
+        print("len(backloggery_backup)", len(backloggery_backup))
+        print("len(backloggery_backup[0])", len(backloggery_backup[0]))
+#         print(backloggery_backup[0][:10])
+
+        x = 0
+        currentConsole = ""
+        while x < len(backloggery_backup):
+
+            currentLine = backloggery_backup[x].strip()
+            print(x, ".\t", currentLine)
+
+            # Get console
+            if 'class="gamebox systemend"' in currentLine:
+                if currentLine.startswith('</section>'):
+                    currentLine = currentLine[10:]
+                currentLine = currentLine.split('>')
+                currentConsole = currentLine[3].split('<')[0]
+                print("\n\nConsole is:", currentConsole)
+
+            # Get status
+            elif 'alt="(' in currentLine:
+                completionStatus = currentLine.split('(')[1].split(')')[0]
+                print("\nStatus is:", completionStatus)
+
+            # Get name
+            elif currentLine.startswith('<b>') and currentLine.endswith('</b>'):
+                cutoff = len(currentLine) - 4
+                name = currentLine[3:cutoff]
+                print("\nName is:", name)
+
+            x += 1
